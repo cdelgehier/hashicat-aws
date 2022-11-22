9cd0349b85d8edab9742ae30ce6003eca13e1e92
Cédric DELGEHIER <cedric.delgehier@laposte.net>
2022-11-22 14:41:55 +0000

    add tag billable

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,12 +1,18 @@
 module "s3_bucket" {
-  source = "terraform-aws-modules/s3-bucket/aws"
+  source  = "app.terraform.io/chip-cdelgehier/s3-bucket/aws"
+  version = "2.8.0"
 
   bucket_prefix = "cdelgehier"
-  bucket = "my-s3-bucket"
+  #bucket = "my-s3-bucket"
   acl    = "private"
 
   versioning = {
     enabled = true
+  }
+
+  tags = {
+    Department = "devops"
+    Billable = "devops"
   }
 
 }
